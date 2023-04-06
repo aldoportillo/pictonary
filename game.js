c75a1ab0fd57fa432df6d408637ff1ea3506d8a0
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-04-05 19:52:56 -0500

    Create scene init function, create scene function and update scene  function

--- game.js
+++ game.js
@@ -15,13 +15,37 @@ class Game {
         };
     }
 
-    async initScene(data) { }
-    async createScene() { }
-    async updateScene() { }
+    async initScene(data) { 
+        //Will initialize database info here
+        this.strokes = [];
+        this.isDrawing = false;
+    }
+    async createScene() { 
+        //Will create scene in database here
+        this.graphics = this.add.graphics();
+        //sets line to 4 px in size and color green
+        this.graphics.lineStyle(4, 0x00aa00);
+    }
+    async updateScene() { 
+        if(!this.input.activePointer.isDown && this.isDrawing){
+            this.isDrawing = false;
+        } else if(this.input.activePointer.isDown){
+            if(!this.isDrawing){
+                this.path = new Phaser.Curves.Path(this.input.activePointer.position.x - 2, this.input.activePointer.position.y -2);
+                this.isDrawing = true;
+            } else {
+                this.path.lineTo(this.input.activePointer.position.x - 2, this.input.activePointer.position.y -2);
+            }
+            this.path.draw(this.graphics)
+        }
+    }
 
     async authenticate() { }
     async joinOrCreateGame(id) { }
     async joinGame(id, authId) { }
-    async createGame(id, authId) { }
+    async createGame(id, authId) { 
+        this.game = new Phaser.Game(this.phaserConfig);
+        this.game.scene.start("default", {});
+     }
 
 }
